0c7fae3ac396252fd77fd75dbefa0e8b87567588
user <user@com2pc22>
2018-05-31 04:42:45 -0600

    Crear un hola mundodd

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class HolaMundo {
+
+   
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null,"¡HOLA MUNDO!");
+    }
+    
+}
